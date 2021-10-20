4592e8ca45564eb9d7352fc5686e3f698d584ca6
googray <83138023+googray@users.noreply.github.com>
2021-10-20 22:43:20 +0200

    feat: update html & css, js trying to make a next row function

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
-const containerTodoRow = document.querySelector(".todo-input");
-console.log(containerTodoRow);
+const containerTodoMaker = document.querySelector(".todo-input");
+// console.log(containerTodoMaker);
+const containerTodoEdit = document.querySelector(".todo-input .edit");
+const containerTodoRow = document.querySelector(".todo-task");
 
 const btnTodo = document.querySelector(".btn-add");
-const todo = containerTodoRow.value;
-console.log(todo);
+const btnUpdate = document.querySelector(".btn-edit");
+// const tagClose
+// const tagEdit
 
-const displayTodo = function (todo) {
-  containerTodoRow.innerHTML = "";
+const displayTodo = function (text) {
+  containerTodoMaker.innerHTML = "";
 
   //   todo.forEach(function (tdo, i) {
   const html = `
         <div class="todo-row">
-            <div class="todo-task">QWERTYU</div>
+            <div class="todo-task">${text}</div>
             <div class="icons">
               <svg class="delete-icon"></svg>
               <svg class="edit-icon"></svg>
             </div>
           </div>
         `;
-  containerTodoRow.insertAdjacentHTML("afterbegin", html);
+  containerTodoMaker.insertAdjacentHTML("afterbegin", html);
   //   });
 };
 
 btnTodo.addEventListener("click", function (e) {
   e.preventDefault();
   console.log("DONE");
-  displayTodo();
+  const text = containerTodoMaker.value;
+  console.log(text);
+  displayTodo(text);
   //   updateUI(currentAccount);
 });
 
