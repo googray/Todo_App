2ef2c5312eacded8ef965b39a06aa56fedeba2a5
googray <shat.rom07@gmail.com>
2021-11-03 13:54:08 +0100

    feat: option delete & check, but they dosnt work

--- app.js
+++ app.js
@@ -4,15 +4,19 @@ const containerTodoEdit = document.querySelector(".todo-input .edit");
 const containerTodoRow = document.querySelector(".todo-task");
 
 const btnTodo = document.querySelector(".btn-add");
-const btnUpdate = document.querySelector(".btn-edit");
-const btnRemove = document.querySelector(".delete-icon");
+const update = document.querySelector(".btn-edit");
+
 const todoContent = document.querySelector(".todo-content");
 const todoField = document.querySelector(".todo-field");
+
+const input = document.querySelector(".todo-input");
+const row = document.querySelector(".todo-row");
 // const tagClose
 // const tagEdit
 
-function displayTodo() {
+function displayTodo(e) {
   // containerTodoMaker.innerHTML = "";
+  e.preventDefault();
   const html = `
            <div class="todo-row">
                <div class="todo-task">sadfgh</div>
@@ -23,11 +27,31 @@ function displayTodo() {
              </div>
            `;
   document.querySelector(".todo-field").insertAdjacentHTML("beforeend", html);
-}
 
-function removeTodo(input) {
-  input.parentNode.remove();
+  const remove = document.getElementsByClassName(".delete-icon");
+  let i;
+  for (i = 0; i < remove.length; i++) {
+    remove[i].onclick = function () {
+      let div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
 }
 
 btnTodo.addEventListener("click", displayTodo);
-// btnRemove.addEventListener("click", removeTodo);
+
+function check(e) {
+  if (e.target.className === ".todo-row") {
+    e.target.classList.toggle("checked");
+  }
+}
+todoField.addEventListener("click", check);
+
+// const remove = document.querySelector(".delete-icon");
+// let i;
+// for (i = 0; i < remove.length; i++) {
+//   remove[i].onclick = function () {
+//     let div = this.parentElement;
+//     div.style.display = "none";
+//   };
+// }
